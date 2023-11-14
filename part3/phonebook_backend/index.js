c7019abf1cca43c42eb3775f2cd0d22e6d2f4780
Jay <japusen32@gmail.com>
2023-11-13 18:25:38 -0800

    Part 3b Exercises 3.9 - 3.11

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.static("dist"));
 
 let persons = [
 	{
@@ -81,5 +85,5 @@ app.post("/api/persons", (request, response) => {
 	response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
