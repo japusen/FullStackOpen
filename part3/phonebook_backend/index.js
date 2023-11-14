fd96ff1dee71768a528a79251d46d96a15852e84
Jay <japusen32@gmail.com>
2023-11-13 20:43:09 -0800

    Part 3c Exercises 3.15 - 3.18

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -5,10 +5,32 @@ const morgan = require("morgan");
 const cors = require("cors");
 const Person = require("./models/person");
 
-app.use(express.json());
-app.use(morgan("tiny"));
-app.use(cors());
+const requestLogger = (request, response, next) => {
+	console.log("Method:", request.method);
+	console.log("Path:  ", request.path);
+	console.log("Body:  ", request.body);
+	console.log("---");
+	next();
+};
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	if (error.name === "CastError") {
+		return response.status(400).send({ error: "malformatted id" });
+	}
+
+	next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: "unknown endpoint" });
+};
+
 app.use(express.static("dist"));
+app.use(express.json());
+app.use(cors());
+app.use(requestLogger);
 
 app.get("/info", (request, response) => {
 	Person.find({}).then((persons) => {
@@ -26,16 +48,42 @@ app.get("/api/persons", (request, response) => {
 	});
 });
 
-app.get("/api/persons/:id", (request, response) => {
-	Person.findById(request.params.id).then((person) => {
-		response.json(person);
-	});
+app.get("/api/persons/:id", (request, response, next) => {
+	Person.findById(request.params.id)
+		.then((person) => {
+			if (person) {
+				response.json(person);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {});
+app.delete("/api/persons/:id", (request, response, next) => {
+	Person.findByIdAndDelete(request.params.id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+	const body = request.body;
+
+	const person = {
+		name: body.name,
+		number: body.number,
+	};
+
+	Person.findByIdAndUpdate(request.params.id, person, { new: true })
+		.then((updatedPerson) => {
+			response.json(updatedPerson);
+		})
+		.catch((error) => next(error));
+});
 
 app.post("/api/persons", (request, response) => {
-	const id = Math.random() * 100000;
 	const body = request.body;
 
 	if (!body.name || !body.number) {
@@ -53,6 +101,10 @@ app.post("/api/persons", (request, response) => {
 		response.json(savedPerson);
 	});
 });
+
+app.use(unknownEndpoint);
+// this has to be the last loaded middleware.
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT);
