c4df810b7b72ae668dce5bf599bc5df0cdc3f5a8
Jay <japusen32@gmail.com>
2023-11-13 20:02:04 -0800

    Part 3c Exercises 3.13 - 3.14

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,65 +1,38 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.static("dist"));
 
-let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: 4,
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
-];
-
 app.get("/info", (request, response) => {
-	response.send(
-		`<p>Phonebook has info for ${
-			persons.length
-		} people</p><p>${new Date().toLocaleTimeString()}</p>`
-	);
+	Person.find({}).then((persons) => {
+		response.send(
+			`<p>Phonebook has info for ${
+				persons.length
+			} people</p><p>${new Date().toLocaleTimeString()}</p>`
+		);
+	});
 });
 
 app.get("/api/persons", (request, response) => {
-	response.json(persons);
+	Person.find({}).then((persons) => {
+		response.json(persons);
+	});
 });
 
 app.get("/api/persons/:id", (request, response) => {
-	const id = Number(request.params.id);
-	const person = persons.find((p) => p.id === id);
-
-	if (!person) {
-		response.send(404).end();
-	} else {
-		response.send(person);
-	}
+	Person.findById(request.params.id).then((person) => {
+		response.json(person);
+	});
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-	const id = Number(request.params.id);
-	persons = persons.filter((p) => p.id !== id);
-
-	response.status(204).end();
-});
+app.delete("/api/persons/:id", (request, response) => {});
 
 app.post("/api/persons", (request, response) => {
 	const id = Math.random() * 100000;
@@ -69,21 +42,18 @@ app.post("/api/persons", (request, response) => {
 		return response.status(400).json({
 			error: "content missing",
 		});
-	} else if (persons.find((p) => p.name === body.name)) {
-		return response.status(400).json({
-			error: "person already exists",
-		});
 	}
 
-	const person = {
+	const person = new Person({
 		name: body.name,
 		number: body.number,
-		id: id,
-	};
+	});
 
-	persons = persons.concat(person);
-	response.json(person);
+	person.save().then((savedPerson) => {
+		response.json(savedPerson);
+	});
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
