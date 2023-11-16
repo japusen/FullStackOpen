bc2d15b7ade115f752634d802b6e86d7369e8f4f
Jay <japusen32@gmail.com>
2023-11-16 15:05:58 -0800

    Part 4b Exercises 4.13 - 4.14

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -15,74 +15,110 @@ beforeEach(async () => {
 	await Promise.all(promiseArray);
 }, 100000);
 
-test("blogs are returned as json", async () => {
-	await api
-		.get("/api/blogs")
-		.expect(200)
-		.expect("Content-Type", /application\/json/);
+describe("when there are initially some blogs saved", () => {
+	test("blogs are returned as json", async () => {
+		await api
+			.get("/api/blogs")
+			.expect(200)
+			.expect("Content-Type", /application\/json/);
+	});
+
+	test("all blogs are returned", async () => {
+		const response = await api.get("/api/blogs");
+
+		expect(response.body).toHaveLength(helper.initialBlogs.length);
+	});
+
+	test("unique identifier property is named 'id'", async () => {
+		const response = await api.get("/api/blogs");
+		const firstNote = response.body[0];
+		expect(firstNote.id).toBeDefined();
+	});
 });
 
-test("all blogs are returned", async () => {
-	const response = await api.get("/api/blogs");
+describe("addition of a new note", () => {
+	test("succeeds with valid data", async () => {
+		const newBlog = {
+			title: "async/await simplifies making async calls",
+			url: "test.com",
+		};
 
-	expect(response.body).toHaveLength(helper.initialBlogs.length);
+		await api
+			.post("/api/blogs")
+			.send(newBlog)
+			.expect(201)
+			.expect("Content-Type", /application\/json/);
+
+		const blogsAfter = await helper.blogsInDb();
+		expect(blogsAfter).toHaveLength(helper.initialBlogs.length + 1);
+
+		const titles = blogsAfter.map((b) => b.title);
+		expect(titles).toContain("async/await simplifies making async calls");
+	});
+
+	test("defaults to 0 likes if likes is missing", async () => {
+		const newBlog = {
+			title: "async/await simplifies making async calls",
+			url: "test.com",
+		};
+
+		const response = await api
+			.post("/api/blogs")
+			.send(newBlog)
+			.expect(201)
+			.expect("Content-Type", /application\/json/);
+
+		const savedBlog = response.body;
+		expect(savedBlog.likes).toEqual(0);
+	});
+
+	test("fails if title and/or url are missing", async () => {
+		const missingTitle = {
+			url: "test.com",
+		};
+
+		const missingURL = {
+			title: "async/await simplifies making async calls",
+		};
+
+		const missingTitleAndURL = {};
+
+		await api.post("/api/blogs").send(missingTitle).expect(400);
+		await api.post("/api/blogs").send(missingURL).expect(400);
+		await api.post("/api/blogs").send(missingTitleAndURL).expect(400);
+	});
 });
 
-test("unique identifier property is named 'id'", async () => {
-	const response = await api.get("/api/blogs");
-	const firstNote = response.body[0];
-	expect(firstNote.id).toBeDefined();
+describe("deletion of a blog", () => {
+	test("succeeds with a valid id", async () => {
+		const blogs = await helper.blogsInDb();
+		const blogToDelete = blogs[0];
+
+		await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+		const blogsAfter = await helper.blogsInDb();
+		expect(blogsAfter).toHaveLength(helper.initialBlogs.length - 1);
+
+		const titles = blogsAfter.map((b) => b.title);
+		expect(titles).not.toContain(blogToDelete.title);
+	});
 });
 
-test("a valid blog can be added", async () => {
-	const newBlog = {
-		title: "async/await simplifies making async calls",
-		url: "test.com",
-	};
+describe("updating a blog", () => {
+	test("by changing the number of likes", async () => {
+		const blogs = await helper.blogsInDb();
+		const updatedBlog = { ...blogs[0], likes: 999 };
 
-	await api
-		.post("/api/blogs")
-		.send(newBlog)
-		.expect(201)
-		.expect("Content-Type", /application\/json/);
+		const response = await api
+			.put(`/api/blogs/${updatedBlog.id}`)
+			.send(updatedBlog);
 
-	const blogsAfter = await helper.blogsInDb();
-	expect(blogsAfter).toHaveLength(helper.initialBlogs.length + 1);
+		const returnedNote = response.body;
+		expect(returnedNote.likes).toEqual(999);
 
-	const titles = blogsAfter.map((b) => b.title);
-	expect(titles).toContain("async/await simplifies making async calls");
-});
-
-test("number of likes defaults to 0", async () => {
-	const newBlog = {
-		title: "async/await simplifies making async calls",
-		url: "test.com",
-	};
-
-	const response = await api
-		.post("/api/blogs")
-		.send(newBlog)
-		.expect(201)
-		.expect("Content-Type", /application\/json/);
-
-	const savedBlog = response.body;
-	expect(savedBlog.likes).toEqual(0);
-});
-
-test("missing title or url", async () => {
-	const missingTitle = {
-		url: "test.com",
-	};
-
-	const missingURL = {
-		title: "async/await simplifies making async calls",
-	};
-
-	const missingTitleAndURL = {};
-
-	await api.post("/api/blogs").send(missingTitle).expect(400);
-	await api.post("/api/blogs").send(missingURL).expect(400);
-	await api.post("/api/blogs").send(missingTitleAndURL).expect(400);
+		const blogsAfter = await helper.blogsInDb();
+		expect(blogsAfter).toHaveLength(helper.initialBlogs.length);
+	});
 });
 
 afterAll(async () => {
