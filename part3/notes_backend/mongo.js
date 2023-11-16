fb0ea351f835ec0580425a72734956473579a178
Jay <japusen32@gmail.com>
2023-11-16 14:25:36 -0800

    Part 4b

--- part3/notes_backend/mongo.js
+++ part3/notes_backend/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://fullstackopen:${password}@cluster0.ksn2elb.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://fullstackopen:${password}@cluster0.ksn2elb.mongodb.net/testNoteApp?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
@@ -19,19 +19,29 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema);
 
-// const note = new Note({
-// 	content: "Mongoose makes things easy",
-// 	important: true,
-// });
+const note = new Note({
+	content: "HTML is easy",
+	important: true,
+});
 
-// note.save().then((result) => {
-// 	console.log("note saved!");
-// 	mongoose.connection.close();
-// });
-
-Note.find({ important: true }).then((result) => {
-	result.forEach((note) => {
-		console.log(note);
-	});
+note.save().then((result) => {
+	console.log("note saved!");
 	mongoose.connection.close();
 });
+
+const note2 = new Note({
+	content: "Mongoose makes things easy",
+	important: true,
+});
+
+note2.save().then((result) => {
+	console.log("note saved!");
+	mongoose.connection.close();
+});
+
+// Note.find({ important: true }).then((result) => {
+// 	result.forEach((note) => {
+// 		console.log(note);
+// 	});
+// 	mongoose.connection.close();
+// });
