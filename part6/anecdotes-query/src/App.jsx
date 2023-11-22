7e265ec7d4044d5139325e2b57063e117c82cf3b
Jay <japusen32@gmail.com>
2023-11-22 14:43:17 -0800

    Exercise 6.21

--- part6/anecdotes-query/src/App.jsx
+++ part6/anecdotes-query/src/App.jsx
@@ -1,19 +1,26 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAnecdotes, updateAnecdote } from "./requests";
 
 const App = () => {
+	const queryClient = useQueryClient();
+	const voteMutation = useMutation({
+		mutationFn: updateAnecdote,
+		onSuccess: () =>
+			queryClient.invalidateQueries({ queryKey: ["anecdotes"] }),
+	});
+
 	const handleVote = (anecdote) => {
-		console.log("vote");
+		voteMutation.mutate({
+			...anecdote,
+			votes: anecdote.votes + 1,
+		});
 	};
 
 	const result = useQuery({
 		queryKey: ["anecdotes"],
-		queryFn: () =>
-			axios
-				.get("http://localhost:3001/anecdotes")
-				.then((res) => res.data),
+		queryFn: getAnecdotes,
 	});
 
 	if (result.isLoading) {
--- part6/anecdotes-query/src/components/AnecdoteForm.jsx
+++ part6/anecdotes-query/src/components/AnecdoteForm.jsx
@@ -1,21 +1,30 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { creatAnecdote } from "../requests";
+
 const AnecdoteForm = () => {
+	const queryClient = useQueryClient();
+	const newAnecdoteMutation = useMutation({
+		mutationFn: creatAnecdote,
+		onSuccess: () =>
+			queryClient.invalidateQueries({ queryKey: ["anecdotes"] }),
+	});
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+	const onCreate = async (event) => {
+		event.preventDefault();
+		const content = event.target.anecdote.value;
+		event.target.anecdote.value = "";
+		newAnecdoteMutation.mutate({ content, votes: 0 });
+	};
 
-  return (
-    <div>
-      <h3>create new</h3>
-      <form onSubmit={onCreate}>
-        <input name='anecdote' />
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h3>create new</h3>
+			<form onSubmit={onCreate}>
+				<input name="anecdote" />
+				<button type="submit">create</button>
+			</form>
+		</div>
+	);
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
