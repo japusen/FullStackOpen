af34da4945817c0d04cd64ade411b92760cc4cea
Jay <japusen32@gmail.com>
2023-11-30 20:28:43 -0800

    Part 9d

---
+++ part9/flight_diaries/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import diaryRouter from "./routes/diaries";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = 3000;
+
+app.use("/api/diaries", diaryRouter);
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
