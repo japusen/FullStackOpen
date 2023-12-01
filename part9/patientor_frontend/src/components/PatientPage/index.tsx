37a74efa2841ed13126f03c9ba5f4a0c554bd23c
Jay <japusen32@gmail.com>
2023-11-30 23:13:28 -0800

    Exercise 9.24

--- part9/patientor_frontend/src/components/PatientPage/index.tsx
+++ part9/patientor_frontend/src/components/PatientPage/index.tsx
@@ -1,11 +1,70 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Patient } from "../../types";
+import { Patient, Entry, Diagnosis } from "../../types";
 import patientService from "../../services/patients";
+import diagnosesService from "../../services/diagnoses";
+
+const EntryView = ({
+	entry,
+	diagnoses,
+}: {
+	entry: Entry;
+	diagnoses: Diagnosis[];
+}) => {
+	return (
+		<div>
+			<p>
+				{entry.date} <em>{entry.description}</em>
+			</p>
+			{entry.diagnosisCodes ? (
+				<ul>
+					{entry.diagnosisCodes.map((code) => (
+						<DiagnosisView
+							key={code}
+							code={code}
+							diagnoses={diagnoses}
+						/>
+					))}
+				</ul>
+			) : (
+				<></>
+			)}
+		</div>
+	);
+};
+
+const DiagnosisView = ({
+	code,
+	diagnoses,
+}: {
+	code: string;
+	diagnoses: Diagnosis[];
+}) => {
+	const diagnosis = diagnoses.find((diagnosis) => diagnosis.code === code);
+
+	return (
+		<li key={code}>
+			{code} {diagnosis ? diagnosis.name : null}
+		</li>
+	);
+};
+
+const PatientInfo = ({ patient }: { patient: Patient }) => {
+	return (
+		<div>
+			<h2>{patient.name}</h2>
+			{patient.dateOfBirth && <p>DOB: {patient.dateOfBirth}</p>}
+			<p>Gender: {patient.gender}</p>
+			{patient.ssn && <p>ssn: {patient.ssn}</p>}
+			<p>Occupation: {patient.occupation}</p>
+		</div>
+	);
+};
 
 const PatientPage = () => {
 	const id = useParams().id;
 	const [patient, setPatient] = useState<Patient>();
+	const [diagnoses, setDiagnoses] = useState<Diagnosis[]>();
 
 	useEffect(() => {
 		const fetchPatient = async () => {
@@ -14,35 +73,27 @@ const PatientPage = () => {
 				setPatient(patient);
 			}
 		};
-		void fetchPatient();
+		const fetchDiagnoses = async () => {
+			const diagnoses = await diagnosesService.getAll();
+			setDiagnoses(diagnoses);
+		};
+		fetchPatient();
+		fetchDiagnoses();
 	}, [id]);
 
 	return (
 		<div>
-			{patient ? (
+			{patient && diagnoses ? (
 				<>
-					<h2>{patient.name}</h2>
-					{patient.dateOfBirth && <p>DOB: {patient.dateOfBirth}</p>}
-					<p>Gender: {patient.gender}</p>
-					{patient.ssn && <p>ssn: {patient.ssn}</p>}
-					<p>Occupation: {patient.occupation}</p>
-					<h3>entries</h3>
+					<PatientInfo patient={patient} />
 
+					<h3>entries</h3>
 					{patient.entries.map((entry) => (
-						<div key={entry.id}>
-							<p>
-								{entry.date} <em>{entry.description}</em>
-							</p>
-							{entry.diagnosisCodes ? (
-								<ul>
-									{entry.diagnosisCodes.map((code) => (
-										<li key={code}>{code}</li>
-									))}
-								</ul>
-							) : (
-								<></>
-							)}
-						</div>
+						<EntryView
+							key={entry.id}
+							entry={entry}
+							diagnoses={diagnoses}
+						/>
 					))}
 				</>
 			) : (
