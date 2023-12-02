6008165fdd7102407cbc13c79da4fce0c33f9918
Jay <japusen32@gmail.com>
2023-12-01 20:51:08 -0800

    Part 8b

---
+++ part8/part-b/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import Persons from "./components/Persons";
+import PersonForm from "./components/PersonForm";
+import Notify from "./components/Notify";
+import { ALL_PERSONS } from "./queries";
+import PhoneForm from "./components/PhoneForm";
+
+const App = () => {
+	const [errorMessage, setErrorMessage] = useState(null);
+	const result = useQuery(ALL_PERSONS);
+
+	const notify = (message) => {
+		setErrorMessage(message);
+		setTimeout(() => {
+			setErrorMessage(null);
+		}, 10000);
+	};
+
+	if (result.loading) {
+		return <div>loading...</div>;
+	}
+
+	return (
+		<div>
+			<Notify errorMessage={errorMessage} />
+			<Persons persons={result.data.allPersons} />
+			<PersonForm setError={notify} />
+			<PhoneForm setError={notify} />
+		</div>
+	);
+};
+
+export default App;
