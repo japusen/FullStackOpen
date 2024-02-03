f7ee1766cd625a6d75721f5d61177bb90b7a7214
Jay <japusen32@gmail.com>
2024-02-02 22:27:55 -0800

    Exercise 10.13

--- part10/rate-repository-app/src/components/SignIn.jsx
+++ part10/rate-repository-app/src/components/SignIn.jsx
@@ -4,6 +4,7 @@ import FormikTextInput from "./FormikTextInput";
 import { Formik } from "formik";
 import theme from "../theme";
 import * as yup from "yup";
+import useSignIn from "../hooks/useSignIn";
 
 const initialValues = {
 	username: "",
@@ -58,9 +59,17 @@ const SignInForm = ({ onSubmit }) => {
 };
 
 const SignIn = () => {
-	const onSubmit = (values) => {
-		console.log(values);
+	const [signIn, result] = useSignIn();
+	const onSubmit = async (values) => {
+		const { username, password } = values;
+		try {
+			await signIn({ username, password });
+			console.log(result.data);
+		} catch (e) {
+			console.log(e);
+		}
 	};
+
 	return (
 		<Formik
 			initialValues={initialValues}
---
+++ part10/rate-repository-app/src/graphql/mutations.js
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client";
+
+export const SIGN_IN = gql`
+	mutation SignIn($username: String!, $password: String!) {
+		authenticate(
+			credentials: { username: $username, password: $password }
+		) {
+			accessToken
+		}
+	}
+`;
